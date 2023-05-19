2ab752148f6583dd5c41deaaa5542460f393ab5e
Tong LI <tl10@sanger.ac.uk>
2023-05-19 16:49:31 +0000

    working version of the pipeline on Farm

--- lsf_run.sh
+++ lsf_run.sh
@@ -6,13 +6,11 @@
 # Distributed under terms of the BSD-3 license.
 #
 
-PATH="/software/singularity-v3.6.4/bin/":$PATH
 MOUNT_POINT='/lustre/scratch117/cellgen/team283/tl10/NXF_WORK/'
 
-DATE_WITH_TIME=`date "+%Y%m%d%H%M"`
-TMP_NF_WORK="$MOUNT_POINT/${USER}_${DATE_WITH_TIME}_segmentation_work"
+TMP_NF_WORK="$MOUNT_POINT/${USER}_segmentation_work"
 
-NXF_OPTS='-Dleveldb.mmap=false' NXF_WORK=$TMP_NF_WORK LSB_DEFAULTGROUP='team283' /lustre/scratch117/cellgen/team283/tl10/nextflow/nextflow -trace nextflow.executor run /lustre/scratch117/cellgen/team283/tl10/workflow-segmentation/main.nf \
+NXF_OPTS='-Dleveldb.mmap=false' NXF_WORK=$TMP_NF_WORK NXF_VER="22.04.5" LSB_DEFAULTGROUP='team283' nextflow -trace nextflow.executor run /lustre/scratch126/cellgen/team283/tl10/workflow-segmentation/main.nf \
 	-params-file $1 \
 	-profile lsf \
 	-entry run_nuc_seg \
