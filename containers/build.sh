aed9be7ba931ce6788fa24cd5330ab71c9638f08
Tong LI <tl10@sanger.ac.uk>
2023-06-17 15:12:08 +0000

    container setting update

--- containers/build.sh
+++ containers/build.sh
@@ -1,1 +1,2 @@
 docker build -t workflow-segmentation:latest .
+singularity build /lustre/scratch126/cellgen/team283/imaging_sifs/workflow-segmentation.sif docker-daemon://workflow-segmentation:latest
