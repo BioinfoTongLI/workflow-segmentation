74bc654e177a9bc8037b374fe08fb3c3c9573998
Tong LI <tl10@sanger.ac.uk>
2023-10-08 23:37:09 +0000

    add normalisation back; handle Z_max defalt parameter

--- bin/cellpose_3d_seg.py
+++ bin/cellpose_3d_seg.py
@@ -20,10 +20,35 @@ def cellpose_seg_3d(chunk, model, diam=30, cellprob_threshold=0.0, chs=[2, 1]):
     return masks
 
 
+def normalize_image_stack_slice_by_slice(stack):
+    """
+    Normalize a 3D image stack slice by slice using NumPy.
+
+    Parameters:
+    stack (numpy.ndarray): A 3D image stack.
+
+    Returns:
+    numpy.ndarray: The normalized 3D image stack.
+    """
+    # Get the shape of the stack
+    z, y, x = stack.shape
+
+    # Normalize the stack slice by slice
+    for i in range(z):
+        img = stack[i, :, :]
+        mean = np.mean(img)
+        std = np.std(img)
+        img = (img - mean) / std
+        p1, p2 = np.percentile(img, (0, 100))
+        p1, p2 = tuple([p1, p2])
+        stack[i, :, :] = exposure.rescale_intensity(img, in_range=(p1, p2), out_range=(0, 255))
+    return stack
+
+
 # segment cells with canonical cellpose API
 def segment(stem:str, img_p:str, chs=[0, 0], s=0,
             diameter=30, cellprob_threshold=0.0, C=0,
-            Z_min=0, Z_max=1, T_min=0, T_max=-1, normalize=False):
+            Z_min=0, Z_max=-1, T_min=0, T_max=-1, normalize=False):
     chs_str=",".join([str(chs[0]), str(chs[1])])
     img = AICSImage(img_p)
     img.set_scene(s)
@@ -32,12 +57,15 @@ def segment(stem:str, img_p:str, chs=[0, 0], s=0,
                        append=True, bigtiff=True) as tif:
 
         T_max = img.dims.T if T_max == -1 else T_max
+        Z_max = img.dims.Z if Z_max == -1 else Z_max
         print(img.shape)
 
         for t in range(T_min, T_max):
             stack = img.get_image_dask_data(
                 "ZYX", T=t, C=C, Z=np.arange(Z_min, Z_max)
             )
+            if bool(normalize):
+                stack = normalize_image_stack_slice_by_slice(stack)
             seg = cellpose_seg_3d(
                     stack.compute(),
                     model, chs=chs, diam=diameter,
