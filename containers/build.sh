a035477c25a3be94395dbc15cdbc4397d0b00f71
Tong LI <tl10@sanger.ac.uk>
2023-10-12 16:00:16 +0000

    update container

--- containers/build.sh
+++ containers/build.sh
@@ -1,2 +1,2 @@
-docker build -t workflow-segmentation:latest .
-singularity build /lustre/scratch126/cellgen/team283/imaging_sifs/workflow-segmentation.sif docker-daemon://workflow-segmentation:latest
+docker build -t bioinfotongli/workflow-segmentation:latest .
+singularity build /lustre/scratch126/cellgen/team283/imaging_sifs/bioinfotongli-workflow-segmentation-latest.sif docker-daemon://bioinfotongli/workflow-segmentation:latest
