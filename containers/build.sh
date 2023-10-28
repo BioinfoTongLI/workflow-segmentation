ab424db8210f20099b5e392f8f7b8ce71e49f9ef
Tong LI <tl10@sanger.ac.uk>
2023-10-28 13:48:15 +0000

    do not explicitly build singularity container anymore

--- containers/build.sh
+++ containers/build.sh
@@ -1,2 +1,1 @@
 docker build -t bioinfotongli/workflow-segmentation:latest .
-singularity build /lustre/scratch126/cellgen/team283/imaging_sifs/bioinfotongli-workflow-segmentation-latest.sif docker-daemon://bioinfotongli/workflow-segmentation:latest
